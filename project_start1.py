527e33364aa9727a27c2b1f4326b1fce1e5e632c
marco <vrban.marco.Git@>
2021-10-04 19:35:52 +0200

    added user_input, tying to add a class Calories.

--- project_start1.py
+++ project_start1.py
@@ -1,3 +1,7 @@
+# class Calories(cal):
+# cal =
+
+
 def weight(kg):
     kg *= 9
     return kg
@@ -10,14 +14,23 @@ def height(cm):
 
 def age(my_age):
     my_age *= 6
+
+    if my_age <= 15:
+        my_age += 6
+
     return my_age
 
 
-
 def main():
-    print(weight(65))
-    print(height(180))
-    print(age(25))
+    print("Welcome to my calorie calculator!")
+    user_weight = (int(input("Please enter your weight:")))
+    user_height = (int(input("Please enter your height:")))
+    user_age = (int(input("Please enter your age:")))
+
+    print("----------------------------")
+
+    print("Maintain weight:", weight(user_weight) + height(user_height) + age(user_age))
+    print("Calories/day")
 
 
 if __name__ == "__main__":
