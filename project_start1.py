63557c60cdac9525f177923da684b628263e0847
marco <vrban.marco.Git@>
2021-10-13 14:47:11 +0200

    fixed def choice, imported Person

--- project_start1.py
+++ project_start1.py
@@ -1,60 +1,58 @@
 
-class Person:
+from person import Person
 
-    def __init__(self, kg, cm, my_age):
-        self.kg = kg
-        self.cm = cm
-        self.my_age = my_age
-        self.cal = None
 
-    def weight(self):
-        self.kg *= 9
-        return self.kg
+def choice(user_choice):
+    synonyms_loss = ["loose", "weight loss", "loose weight", "cut"]
+    synonyms_gain = ["gain", "weight gain", "gain weight", "bulk"]
 
-    def height(self):
-        self.cm *= 7
-        return self.cm
+    if user_choice in synonyms_loss:
+        user_choice = "Weight loss"
+        return user_choice
 
-    def age(self):
-        self.my_age *= 6
-        if self.my_age >= 15:
-            self.my_age += 100
-        return self.my_age
-
-    def calories(self):
-        self.cal = self.weight() + self.height() + self.age()
-        return self.cal
+    else:
+        if user_choice in synonyms_gain:
+            user_choice = "Weight gain"
+            return user_choice
+        return "Invalid choice"
 
 
 def activity(user_training):
     if user_training == 0:
         user_training = "Little to no exercise"
-        print(user_training)
+        return user_training
 
     elif user_training == 1 or user_training <= 3:
-        user_training = "Light exercise"
-        print(user_training)
+        user_training = "Light: exercise"
+        return user_training
 
     elif user_training == 4 or user_training <= 5:
-        user_training = "Moderate exercise"
-        print(user_training)
+        user_training = "Moderate: exercise"
+        return user_training
 
     elif user_training == 6 or user_training <= 7:
         user_training = "Very Active: Intense exercise"
-        print(user_training)
+        return user_training
 
 
 def main():
     print("Welcome to my calorie calculator!")
-    user_training = int(input("How many time per week do you train?:"))
+
+    user_goal = input("Do you like to loose weight or gain weight?:")
+    user_training = int(input("How many times per week do you train?:"))
+
     user_weight = (int(input("Please enter your weight:")))
     user_height = (int(input("Please enter your height:")))
     user_age = (int(input("Please enter your age:")))
+
     user = Person(user_weight, user_height, user_age)
 
     print("----------------------------")
 
+    print(choice(user_goal))
     print(activity(user_training))
+
+    print("----------------------------")
 
     print("Maintain weight:", user.calories())
     print("Calories/day")
